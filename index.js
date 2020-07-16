4899000b00bb72929be630606a1f73eff6a64970
leeks123 <kensei719@naver.com>
2020-07-16 19:45:53 +0900

    second commit

--- index.js
+++ index.js
@@ -3,24 +3,40 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/dev");
+
+//
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://leeks:12341234@cluster0.xxdda.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 //
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! Thank you john"));
+
+app.post("/register", (req, res) => {
+  // 회원가입 할 때 필요한 정보들을 클라이언트에서 가져오면 그것을 DB에 넣어줌
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  }); // 몽고디비에 저장
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
