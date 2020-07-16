6a9ff42209f02b55d6a9eb6345bdb79ba7389d9a
leeks123 <kensei719@naver.com>
2020-07-16 18:52:04 +0900

    first upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// backend start point
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://leeks:12341234@cluster0.xxdda.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+//
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
